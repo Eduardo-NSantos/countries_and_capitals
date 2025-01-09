062db755cfe70796e59a973f8a947b9ca77c825d
Eduardo-SNantos <eduardoshw123@gmail.com>
2025-01-09 11:39:38 -0300

    adição do formulário na view 'home' e validação na MainController

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\View\View;
 
 class MainController extends Controller
 {
@@ -10,11 +11,33 @@ class MainController extends Controller
 
     public function __construct()
     {
+        // carregamento dos dados para o controller
         $this->app_data = require(app_path('app_data.php'));
     }
 
-    public function showData()
+    public function startGame(): View
     {
-        return response()->json($this->app_data);
+        return view('home');
+    }
+
+    public function prepareGame(Request $request)
+    {
+        // validate request
+        $request->validate(
+            [
+                'total_questions' => ['required', 'integer', 'min:3', 'max:30']
+            ],
+            [
+                'total_questions.required' => 'O número de questões é obrigatório',
+                'total_questions.integer' => 'O valor deve ser um número inteiro',
+                'total_questions.min' => 'Escolha no mínimo :min questões',
+                'total_questions.max' => 'Escolha no máximo :max questões'
+            ]
+        );
+
+        // get total questions
+        $total_questions = intval($request->total_questions);
+
+        echo "ok - $total_questions";    
     }
 }
