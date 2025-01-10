5de6750c286f01d0aa0f6811913776309559cb77
Eduardo-SNantos <eduardoshw123@gmail.com>
2025-01-09 21:20:27 -0300

    criação do metodo para analizar a resposta

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\View\View;
 
 class MainController extends Controller
@@ -107,5 +108,45 @@ class MainController extends Controller
             'currentQuestion' => $current_question,
             'answers' => $answers
         ]);
+    }
+
+    public function answer($enc_answer)
+    {
+        try {
+            $answer = Crypt::decryptString($enc_answer);
+        } catch (\Exception $e) {
+            return redirect()->route('game');
+        } 
+
+        // game logic
+        $quiz = session('quiz');
+        $current_question = session('current_question') - 1;
+        $correct_answer = $quiz[$current_question]['correct_answer'];
+        $correct_answers = session('correct_answers');  
+        $wrong_answers = session('wrong_answers');
+
+        if($answer == $correct_answer){
+            $correct_answers++;
+            $quiz[$current_question]['correct'] = true;
+        }else{
+            $wrong_answers++;
+            $quiz[$current_question]['correct'] = false;
+        }
+
+        // update session
+        session([
+            'quiz' => $quiz,
+            'correct_answers' => $correct_answers,
+            'wrong_answers' => $wrong_answers
+        ]);
+
+        // prepare data to show the correct answer
+        $data = [
+            'country' => $quiz[$current_question]['country'],
+            'correct_answer' => $correct_answer,
+            'choice_answer' => $answer,
+            'current_question' => $current_question,
+            'total_questions' => session('total_questions')
+        ];
     }
 }
--- routes/web.php
+++ routes/web.php
@@ -8,4 +8,5 @@ Route::get('/', [MainController::class, 'startGame'])->name('start_game');
 Route::post('/prepare', [MainController::class, 'prepareGame'])->name('prepare_game');
 
 // in game
-Route::get('/game', [MainController::class, 'game'])->name('game');
+Route::get('/game', [MainController::class, 'game'])->name('game'); 
+Route::get('/answer/{answer}', [MainController::class, 'answer'])->name('answer');
